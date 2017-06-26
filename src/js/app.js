8ad03723a753433d11b17e414e1de635e892571d
manegame <manusnijhoff@gmail.com>
2017-06-26 22:33:38 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+(function () {
+
+    "use strict";
+
+    window.$    = require("jquery"),
+    window.jQuery = $;
+
+    require("jquery-ui-dist/jquery-ui.js");
+
+    $(function () {
+
+      console.log('We Live')
+
+    });
+
+}());
