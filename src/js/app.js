defaa986b25f50376760bc9c6d3c8a8de9729159
manegame <manusnijhoff@gmail.com>
2017-06-28 22:52:03 +0200

    a little shitty commity

--- src/js/app.js
+++ src/js/app.js
@@ -9,11 +9,75 @@
 
     require("howler");
 
+    var sound;
+
+    var currentRoom;
+    var counter;
+
     var playBtn = $('#play');
     var pauseBtn = $('#pause');
     var nextBtn = $('#next');
 
+    var r2 = ['magda', 'janne', 'fay', 'noa'];
+    var id;
+
+    // Loading the sounds
+
+    function loadPlaylist() {
+
+      currentRoom = r2;
+      counter = 0;
+
+      console.log(currentRoom[counter]);
+
+      sound = new Howl({
+        src: ['/audio/r2/r2-full.mp3'],
+        sprite: {
+          magda: [0, 15000],
+          fay: [15000, 14000],
+          janne: [29000, 15000],
+          noa: [44000, 13000]
+        },
+        onend: function () {
+          console.log('ended');
+          counter++;
+        }
+      });
+
+      //
+      // event listeners for Howler
+      //
+
+      sound.on('play', function(){
+        console.log('sound playing');
+        nextBtn.removeClass('active');
+        playBtn.removeClass('active');
+        pauseBtn.addClass('active');
+      });
+      sound.on('pause', function(){
+        console.log('sound paused');
+        pauseBtn.removeClass('active');
+        playBtn.addClass('active');
+      });
+      sound.on('end', function(){
+        console.log('sound ended');
+        pauseBtn.removeClass('active');
+        nextBtn.addClass('active');
+      });
+
+    }
+
+    // playing the next sound
+
+    function playNext(room, counter) {
+      id = sound.play(room[counter]);
+      return id;
+    }
+
     $(function () {
+
+      //
+      // Click stuff
 
       $('p, h1, h2, h3, a').on('mousedown touchstart', function() {
         $(this).addClass('mousedown');
@@ -23,48 +87,41 @@
         $(this).removeClass('mousedown');
       });
 
-      var intro = new Howl({
-        src: ['/audio/intro/intro.mp3'],
-        onplay: function(){
-          console.log(playBtn);
-          playBtn.removeClass('active');
-          pauseBtn.addClass('active');
-        },
-        onend: function(){
-          pauseBtn.removeClass('active');
-          playBtn.addClass('active');
-        }
-      });
-
-      var r2 = new Howl({
-        src: ['/audio/r2/r2-full.mp3'],
-        sprite: {
-          magda: [0, 15000],
-          fay: [15000, 14000],
-          janne: [29000, 15000],
-          noa: [41000, 13000]
+      $('.instructions').on('click', function () {
+        if($(this).hasClass('back')){
+          $('.player').removeClass('active');
+          $(this).html('Pick a room to get started');
         }
       });
 
       $('a').on('click', function(event) {
-        event.preventDefault();
+        if(event.target.id == "r1" || event.target.id == "r2" || event.target.id == "r3" || event.target.id == "r4" || event.target.id == "r5" ) {
+          event.preventDefault();
+        }
+
         if(event.target.id == "r2") {
-          console.log('go to room 2');
+          // visual feedback
+          $('#map').attr('src', '/img/floor-plan-r2.svg');
+          $('.r').removeClass('active');
+          $(this).addClass('active');
+          $('.player').addClass('active');
+          $('.instructions').html('Back to overview').addClass('back');
+
+          loadPlaylist();
         }
       });
 
-      $('input').on('click', function(event) {
-        console.log(event.target.id);
-        console.log(event.id);
-        if($(this).hasClass('play')) {
-          intro.play();
-        }
-
+      $('#play').on('click', function() {
+          sound.play('magda');
+          playNext(currentRoom, counter);
       });
-
-      // intro.play();
-
-      // r2.play('magda');
+      $('#pause').on('click', function() {
+          sound.pause(id);
+      });
+      $('#next').on('click', function() {
+          // counter++
+          // var id2 = sound.play('' + r2[counter] + '');
+      });
 
 
     });
