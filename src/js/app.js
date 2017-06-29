6acd152feda1cf4463dd9e4c785bc79f4aba2698
manegame <manusnijhoff@gmail.com>
2017-06-30 00:39:17 +0200

    added database and improved playback qualities

--- src/js/app.js
+++ src/js/app.js
@@ -13,91 +13,193 @@
     var pauseBtn = $('#pause');
     var nextBtn = $('#next');
 
+    // id is based om room + room number + list number
+    //
+
     var r1 = {
 
       "0" : {
         "id"      : "room10",
-        "name"    : "Introduction",
-        "src"     : "/audio/r2/intro.mp3"
+        "name"    : "Charlotte Gramberg",
+        "src"     : "/audio/r1/charlotte.mp3"
       },
 
       "1" : {
         "id"      : "room11",
-        "name"    : "Charlotte Gramberg",
-        "src"     : "/audio/r2/magda.mp3"
+        "name"    : "Jaap Smit",
+        "src"     : "/audio/r1/jaap.mp3"
       },
 
       "2"   : {
         "id"      : "room12",
-        "name"    : "Jaap Smit",
-        "src"     : "/audio/r2/fay.mp3"
+        "name"    : "Wies van der Wal",
+        "src"     : "/audio/r1/wies.mp3"
       },
 
       "3" : {
         "id"      : "room13",
-        "name"    : "Wies van der Wal",
-        "src"     : "/audio/r2/janne.mp3"
+        "name"    : "Yara Veloso",
+        "src"     : "/audio/r1/yara.mp3"
       },
 
       "4"   : {
         "id"      : "room14",
-        "name"    : "Yara Veloso",
-        "src"     : "/audio/r2/noa.mp3"
+        "name"    : "Jamie de Rooij",
+        "src"     : "/audio/r1/jamie.mp3"
       },
 
       "5"   : {
         "id"      : "room15",
-        "name"    : "Jamie de Rooij",
-        "src"     : "/audio/r2/noa.mp3"
-      },
-
-      "6"   : {
-        "id"      : "room16",
         "name"    : "Yacinth Pos",
-        "src"     : "/audio/r2/noa.mp3"
+        "src"     : "/audio/r1/yacinth.mp3"
       }
 
     }
-
-    // id is based om room + room number + list number
-    //
 
     var r2 = {
 
       "0" : {
         "id"      : "room20",
-        "name"    : "Introduction",
-        "src"     : "/audio/r2/intro.mp3"
-      },
-
-      "1" : {
-        "id"      : "room21",
         "name"    : "Magda Skibinska",
         "src"     : "/audio/r2/magda.mp3"
       },
 
-      "2"   : {
-        "id"      : "room22",
+      "1" : {
+        "id"      : "room21",
         "name"    : "Fay Asselbergs",
         "src"     : "/audio/r2/fay.mp3"
       },
 
-      "3" : {
-        "id"      : "room23",
+      "2"   : {
+        "id"      : "room22",
         "name"    : "Janne van Hooff",
         "src"     : "/audio/r2/janne.mp3"
       },
 
-      "4"   : {
-        "id"      : "room24",
+      "3" : {
+        "id"      : "room23",
         "name"    : "Noa Defesche",
         "src"     : "/audio/r2/noa.mp3"
       }
 
     }
 
+    var r3 = {
+
+      "0" : {
+          "id"    : "room30",
+          "name"  : "Antonia Schwaiger",
+          "src"   : "/audio/r3/antonia.mp3"
+      },
+
+      "1" : {
+          "id"    : "room31",
+          "name"  : "Remco Blom",
+          "src"   : "/audio/r3/remco.mp3"
+      },
+
+      "2" : {
+          "id"    : "room32",
+          "name"  : "Caro The",
+          "src"   : "/audio/r3/caro.mp3"
+      },
+
+      "3" : {
+          "id"    : "room33",
+          "name"  : "Jordy Ringeling",
+          "src"   : "/audio/r3/jordyring.mp3"
+      },
+
+      "4" : {
+          "id"    : "room34",
+          "name"  : "Carmel Klein",
+          "src"   : "/audio/r3/carmel.mp3"
+      },
+
+      "5" : {
+          "id"    : "room35",
+          "name"  : "Pascal Schilp",
+          "src"   : "/audio/r3/pascal.mp3"
+      },
+
+      "6" : {
+          "id"    : "room36",
+          "name"  : "Ayse Duran",
+          "src"   : "/audio/r3/ayse.mp3"
+      }
+
+    }
+
+    var r4 = {
+
+      "0" : {
+          "id"    : "room40",
+          "name"  : "Márton Kabai",
+          "src"   : "/audio/r4/marton.mp3"
+      },
+
+      "1" : {
+          "id"    : "room41",
+          "name"  : "Manus Nijhoff",
+          "src"   : "/audio/r4/manus_2.mp3"
+      },
+
+      "2" : {
+          "id"    : "room42",
+          "name"  : "Gregor Nebe",
+          "src"   : "/audio/r4/marton.mp3"
+      },
+
+      "3" : {
+          "id"    : "room43",
+          "name"  : "Jordy van Look",
+          "src"   : "/audio/r4/jordylook.mp3"
+      },
+
+      "4" : {
+          "id"    : "room44",
+          "name"  : "Lisa van den Heuvel",
+          "src"   : "/audio/r4/lisa.mp3"
+      }
+
+    }
+
+    var r5 = {
+
+      "0" : {
+          "id"    : "room50",
+          "name"  : "Ninthe Kiemeneij",
+          "src"   : "/audio/r5/ninthe.mp3"
+      },
+
+      "1" : {
+          "id"    : "room51",
+          "name"  : "Bohye Woo",
+          "src"   : "/audio/r5/bohye.mp3"
+      },
+
+      "2" : {
+          "id"    : "room52",
+          "name"  : "Alice Fialová",
+          "src"   : "/audio/r5/alice.mp3"
+      },
+
+      "3" : {
+          "id"    : "room53",
+          "name"  : "Haewon Lim",
+          "src"   : "/audio/r5/haewon.mp3"
+      },
+
+      "4" : {
+          "id"    : "room54",
+          "name"  : "Geeke van Bruggen",
+          "src"   : "/audio/r5/geeke.mp3"
+      }
+
+    }
+
     var count = 0;
-    var rooms = [r1, r2];
+    var rooms = [r1, r2, r3, r4, r5];
 
     //get amount of items from obj
     Object.size = function(obj) {
@@ -108,16 +210,23 @@
         return size;
     };
 
-    //size of objs
-    var r1Size = Object.size(r1);
-    var r2Size = Object.size(r2);
+        //size of objs
 
-    console.log("r1Size: " + r1Size);
-    console.log("r1Size: " + r1Size);
+    var rSize = [];
+
+    for (var i = 0; i < rooms.length; i ++ ){
+        rSize[i] = Object.size(rooms[i]);
+    }
+
+    console.log(rSize);
+
 
     // to create the play ids on the fly, make these variables
     var room1 = [];
     var room2 = [];
+    var room3 = [];
+    var room4 = [];
+    var room5 = [];
 
     var howls = [];
 
@@ -151,7 +260,7 @@
         // called after picking a room
         // loop over all elements in a database and load them as howler objects
 
-        for (var i = 0; i < rSize; i ++ ) {
+        for (var i = 0; i < rSize[int-1]; i ++ ) {
 
           // how to get the things you need
           // console.log(r2[i].id); // console.log(r2[i].name); // console.log(r2[i].src);
@@ -206,11 +315,20 @@
             pauseBtn.removeClass('active');
             nextBtn.addClass('active');
           });
+          howls[i].on('stop', function(){
+            console.log('sound stopped');
+            pauseBtn.removeClass('active');
+            playBtn.addClass('active');
+          });
 
         }
 
         // event listeners for the controls
         //
+
+        $('.r').on('click', function(){
+          howls[count].stop();
+        });
 
         $('#play').on('click', function() {
             console.log('count: ' +howls[count]);
@@ -234,6 +352,13 @@
 
       }
 
+      function visualFeedback (id, el) {
+        $('#map').attr('src', '/img/floor-plan-'+id+'.svg');
+        $('.r').removeClass('active');
+        el.addClass('active');
+        $('.player').addClass('active');
+        $('.instructions').html('Back to overview').addClass('back');
+      }
 
       // Click stuff
 
@@ -264,26 +389,31 @@
         }
 
         if(event.target.id == "r1") {
-
-          $('#map').attr('src', '/img/floor-plan-r1.svg');
-          $('.r').removeClass('active');
-          $(this).addClass('active');
-          $('.player').addClass('active');
-          $('.instructions').html('Back to overview').addClass('back');
-
-          loadPlaylist(1, r1, room1, r1Size);
+          visualFeedback(event.target.id, $(this));
+          loadPlaylist(1, r1, room1, rSize);
         }
 
         if(event.target.id == "r2") {
-          // visual feedback
-          $('#map').attr('src', '/img/floor-plan-r2.svg');
-          $('.r').removeClass('active');
-          $(this).addClass('active');
-          $('.player').addClass('active');
-          $('.instructions').html('Back to overview').addClass('back');
-
-          loadPlaylist(2, r2, room2, r2Size);
+          visualFeedback(event.target.id, $(this));
+          loadPlaylist(2, r2, room2, rSize);
         }
+
+        if(event.target.id == "r3") {
+          visualFeedback(event.target.id, $(this));
+          loadPlaylist(3, r3, room3, rSize);
+        }
+
+        if(event.target.id == "r4") {
+          visualFeedback(event.target.id, $(this));
+          loadPlaylist(4, r4, room4, rSize);
+        }
+
+        if(event.target.id == "r5") {
+          visualFeedback(event.target.id, $(this));
+          loadPlaylist(5, r5, room5, rSize);
+        }
+
+
       });
 
 
