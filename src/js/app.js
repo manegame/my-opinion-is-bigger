d2f6dbfd25df7a5414100f742df45e302a11ad0e
manegame <manusnijhoff@gmail.com>
2017-06-30 16:24:35 +0200

    added narration

--- src/js/app.js
+++ src/js/app.js
@@ -23,36 +23,72 @@
 
       "0" : {
         "id"      : "room10",
-        "name"    : "Charlotte Gramberg",
-        "src"     : "/audio/r1/charlotte.mp3"
+        "name"    : "...",
+        "src"     : "/audio/r1/0_charlotte.mp3"
       },
 
       "1" : {
         "id"      : "room11",
-        "name"    : "Jaap Smit",
-        "src"     : "/audio/r1/jaap.mp3"
+        "name"    : "Charlotte Gramberg",
+        "src"     : "/audio/r1/charlotte.mp3"
       },
 
-      "2"   : {
+      "2" : {
         "id"      : "room12",
-        "name"    : "Wies van der Wal",
-        "src"     : "/audio/r1/wies.mp3"
+        "name"    : "...",
+        "src"     : "/audio/r1/0_jaap.mp3"
       },
 
       "3" : {
         "id"      : "room13",
-        "name"    : "Yara Veloso",
-        "src"     : "/audio/r1/yara.mp3"
+        "name"    : "Jaap Smit",
+        "src"     : "/audio/r1/jaap.mp3"
       },
 
-      "4"   : {
+      "4" : {
         "id"      : "room14",
-        "name"    : "Jamie de Rooij",
-        "src"     : "/audio/r1/jamie.mp3"
+        "name"    : "...",
+        "src"     : "/audio/r1/0_wies.mp3"
       },
 
       "5"   : {
         "id"      : "room15",
+        "name"    : "Wies van der Wal",
+        "src"     : "/audio/r1/wies.mp3"
+      },
+
+      "6" : {
+        "id"      : "room16",
+        "name"    : "...",
+        "src"     : "/audio/r1/0_yara.mp3"
+      },
+
+      "7" : {
+        "id"      : "room17",
+        "name"    : "Yara Veloso",
+        "src"     : "/audio/r1/yara.mp3"
+      },
+
+      "8" : {
+        "id"      : "room18",
+        "name"    : "...",
+        "src"     : "/audio/r1/0_jamie.mp3"
+      },
+
+      "9"   : {
+        "id"      : "room19",
+        "name"    : "Jamie de Rooij",
+        "src"     : "/audio/r1/jamie.mp3"
+      },
+
+      "10" : {
+        "id"      : "room110",
+        "name"    : "...",
+        "src"     : "/audio/r1/0_yacinth.mp3"
+      },
+
+      "11"   : {
+        "id"      : "room111",
         "name"    : "Yacinth Pos",
         "src"     : "/audio/r1/yacinth.mp3"
       }
@@ -63,24 +99,48 @@
 
       "0" : {
         "id"      : "room20",
-        "name"    : "Magda Skibinska",
-        "src"     : "/audio/r2/magda.mp3"
+        "name"    : "...",
+        "src"     : "/audio/r2/0_magda.mp3"
       },
 
       "1" : {
         "id"      : "room21",
-        "name"    : "Fay Asselbergs",
-        "src"     : "/audio/r2/fay.mp3"
+        "name"    : "Magda Skibinska",
+        "src"     : "/audio/r2/magda.mp3"
       },
 
-      "2"   : {
+      "2" : {
         "id"      : "room22",
-        "name"    : "Janne van Hooff",
-        "src"     : "/audio/r2/janne.mp3"
+        "name"    : "...",
+        "src"     : "/audio/r2/0_fay.mp3"
       },
 
       "3" : {
         "id"      : "room23",
+        "name"    : "Fay Asselbergs",
+        "src"     : "/audio/r2/fay.mp3"
+      },
+
+      "4" : {
+        "id"      : "room24",
+        "name"    : "...",
+        "src"     : "/audio/r2/0_janne.mp3"
+      },
+
+      "5"   : {
+        "id"      : "room25",
+        "name"    : "Janne van Hooff",
+        "src"     : "/audio/r2/janne.mp3"
+      },
+
+      "6" : {
+        "id"      : "room26",
+        "name"    : "...",
+        "src"     : "/audio/r2/0_noa.mp3"
+      },
+
+      "7" : {
+        "id"      : "room27",
         "name"    : "Noa Defesche",
         "src"     : "/audio/r2/noa.mp3"
       }
@@ -90,43 +150,85 @@
     var r3 = {
 
       "0" : {
-          "id"    : "room30",
-          "name"  : "Antonia Schwaiger",
-          "src"   : "/audio/r3/antonia.mp3"
+        "id"      : "room30",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_antonia.mp3"
       },
 
       "1" : {
           "id"    : "room31",
-          "name"  : "Remco Blom",
-          "src"   : "/audio/r3/remco.mp3"
+          "name"  : "Antonia Schwaiger",
+          "src"   : "/audio/r3/antonia.mp3"
       },
 
       "2" : {
-          "id"    : "room32",
-          "name"  : "Caro The",
-          "src"   : "/audio/r3/caro.mp3"
+        "id"      : "room32",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_remco.mp3"
       },
 
       "3" : {
           "id"    : "room33",
-          "name"  : "Jordy Ringeling",
-          "src"   : "/audio/r3/jordyring.mp3"
+          "name"  : "Remco Blom",
+          "src"   : "/audio/r3/remco.mp3"
       },
 
       "4" : {
-          "id"    : "room34",
-          "name"  : "Carmel Klein",
-          "src"   : "/audio/r3/carmel.mp3"
+        "id"      : "room34",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_caro.mp3"
       },
 
       "5" : {
           "id"    : "room35",
+          "name"  : "Caro The",
+          "src"   : "/audio/r3/caro.mp3"
+      },
+
+      "6" : {
+        "id"      : "room36",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_jordyring.mp3"
+      },
+
+      "7" : {
+          "id"    : "room37",
+          "name"  : "Jordy Ringeling",
+          "src"   : "/audio/r3/jordyring.mp3"
+      },
+
+      "8" : {
+        "id"      : "room38",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_carmel.mp3"
+      },
+
+      "9" : {
+          "id"    : "room39",
+          "name"  : "Carmel Klein",
+          "src"   : "/audio/r3/carmel.mp3"
+      },
+
+      "10" : {
+        "id"      : "room310",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_pascal.mp3"
+      },
+
+      "11" : {
+          "id"    : "room311",
           "name"  : "Pascal Schilp",
           "src"   : "/audio/r3/pascal.mp3"
       },
 
-      "6" : {
-          "id"    : "room36",
+      "12" : {
+        "id"      : "room312",
+        "name"    : "...",
+        "src"     : "/audio/r3/0_ayse.mp3"
+      },
+
+      "13" : {
+          "id"    : "room313",
           "name"  : "Ayse Duran",
           "src"   : "/audio/r3/ayse.mp3"
       }
@@ -137,30 +239,60 @@
 
       "0" : {
           "id"    : "room40",
-          "name"  : "Márton Kabai",
-          "src"   : "/audio/r4/marton.mp3"
+          "name"  : "...",
+          "src"   : "/audio/r4/0_marton.mp3"
       },
 
       "1" : {
           "id"    : "room41",
-          "name"  : "Manus Nijhoff",
-          "src"   : "/audio/r4/manus_2.mp3"
+          "name"  : "Márton Kabai",
+          "src"   : "/audio/r4/marton.mp3"
       },
 
       "2" : {
           "id"    : "room42",
-          "name"  : "Gregor Nebe",
-          "src"   : "/audio/r4/gregor.mp3"
+          "name"  : "...",
+          "src"   : "/audio/r4/0_manus.mp3"
       },
 
       "3" : {
           "id"    : "room43",
-          "name"  : "Jordy van Look",
-          "src"   : "/audio/r4/jordylook.mp3"
+          "name"  : "Manus Nijhoff",
+          "src"   : "/audio/r4/manus_2.mp3"
       },
 
       "4" : {
           "id"    : "room44",
+          "name"  : "...",
+          "src"   : "/audio/r4/0_gregor.mp3"
+      },
+
+      "5" : {
+          "id"    : "room45",
+          "name"  : "Gregor Nebe",
+          "src"   : "/audio/r4/gregor.mp3"
+      },
+
+      "6" : {
+          "id"    : "room46",
+          "name"  : "...",
+          "src"   : "/audio/r4/0_jordylook.mp3"
+      },
+
+      "7" : {
+          "id"    : "room47",
+          "name"  : "Jordy van Look",
+          "src"   : "/audio/r4/jordylook.mp3"
+      },
+
+      "8" : {
+          "id"    : "room48",
+          "name"  : "...",
+          "src"   : "/audio/r4/0_lisa.mp3"
+      },
+
+      "9" : {
+          "id"    : "room49",
           "name"  : "Lisa van den Heuvel",
           "src"   : "/audio/r4/lisa.mp3"
       }
@@ -171,30 +303,60 @@
 
       "0" : {
           "id"    : "room50",
-          "name"  : "Ninthe Kiemeneij",
-          "src"   : "/audio/r5/ninthe.mp3"
+          "name"  : "...",
+          "src"   : "/audio/r5/0_ninthe.mp3"
       },
 
       "1" : {
           "id"    : "room51",
-          "name"  : "Bohye Woo",
-          "src"   : "/audio/r5/bohye.mp3"
+          "name"  : "Ninthe Kiemeneij",
+          "src"   : "/audio/r5/ninthe.mp3"
       },
 
       "2" : {
           "id"    : "room52",
-          "name"  : "Alice Fialová",
-          "src"   : "/audio/r5/alice.mp3"
+          "name"  : "...",
+          "src"   : "/audio/r5/0_bohye.mp3"
       },
 
       "3" : {
           "id"    : "room53",
-          "name"  : "Haewon Lim",
-          "src"   : "/audio/r5/haewon.mp3"
+          "name"  : "Bohye Woo",
+          "src"   : "/audio/r5/bohye.mp3"
       },
 
       "4" : {
           "id"    : "room54",
+          "name"  : "...",
+          "src"   : "/audio/r5/0_alice.mp3"
+      },
+
+      "5" : {
+          "id"    : "room55",
+          "name"  : "Alice Fialová",
+          "src"   : "/audio/r5/alice.mp3"
+      },
+
+      "6" : {
+          "id"    : "room56",
+          "name"  : "...",
+          "src"   : "/audio/r5/0_haewon.mp3"
+      },
+
+      "7" : {
+          "id"    : "room57",
+          "name"  : "Haewon Lim",
+          "src"   : "/audio/r5/haewon.mp3"
+      },
+
+      "8" : {
+          "id"    : "room58",
+          "name"  : "...",
+          "src"   : "/audio/r5/0_geeke.mp3"
+      },
+
+      "9" : {
+          "id"    : "room59",
           "name"  : "Geeke van Bruggen",
           "src"   : "/audio/r5/geeke.mp3"
       }
